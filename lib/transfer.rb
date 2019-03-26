fac1c483fb6467567fb46050020fd9fdc7d09c16
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-26 17:01:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,15 +14,13 @@ class Transfer
   end
   
   def execute_transaction
-    for i in 1..1 do 
-      if self.valid? 
-        @sender.balance -= @amount 
-        @receiver.balance += @amount
-        @status = "complete"
-      else 
-        @status = "rejected"
-        "Transaction rejected. Please check your account balance."
-      end
+    if self.valid? 
+      @sender.balance -= @amount 
+      @receiver.balance += @amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
   
